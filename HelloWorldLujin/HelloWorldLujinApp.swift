058f79e02a85cb60ce359d07164b64b29df32900
lujin <lujinmohammed223@gmail.com>
2024-10-20 14:58:11 +0300

    Initial Commit

---
+++ HelloWorldLujin/HelloWorldLujinApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloWorldLujinApp.swift
+//  HelloWorldLujin
+//
+//  Created by lujin mohammed on 17/04/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct HelloWorldLujinApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
